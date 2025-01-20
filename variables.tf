3844711f9f9d2208a6c5fd652b8659b7bd49f1e4
Cristian Beraha <cristian@cloudops.works>
2025-01-20 11:45:08 -0300

    Add descriptions for all variables in variables.tf
    Provide clear and consistent descriptions for each variable, specifying their purpose, type, and default values where applicable. This improves module usability and enhances documentation for end-users.

--- variables.tf
+++ variables.tf
@@ -4,61 +4,74 @@
 #            Distributed Under Apache v2.0 License
 #
 variable "name" {
+  description = "(optional) The name of the account, defaults to: \"\""
   type    = string
   default = ""
 }
 
 variable "organization_name" {
+  description = "(required) The name of the organization"
   type = string
 }
 
 variable "organization_unit" {
+  description = "(required) The organization unit"
   type = string
 }
 
 variable "environment_type" {
+  description = "(required) The environment type"
   type = string
 }
 
 variable "environment_name" {
+  description = "(required) The environment name"
   type = string
 }
 
 variable "organization_email" {
+  description = "(required) The email of the organization"
   type = string
 }
 
 variable "organization_role" {
+  description = "(optional) The role name for default Admin assumerole in organization, defaults to: OrganizationAllowAllAccessAssumeRole"
   type    = string
   default = "OrganizationAllowAllAccessAssumeRole"
 }
 
 variable "organization_allow_billing_access" {
+  description = "(optional) Allow IAM users to access billing, defaults to: true"
   type    = bool
   default = true
 }
 
 variable "organization_parent_id" {
+  description = "(optional) The parent id of the organization, defaults to: null"
   type    = string
   default = null
 }
 
 variable "extra_tags" {
+  description = "(optional) Extra tags to add to the organization, format { key = value }, defaults to: {}"
   type    = map(string)
   default = {}
 }
 
 variable "allow_group" {
+  description = "(optional) The group to allow access to the organization, defaults to: false"
   type    = bool
   default = false
 }
 
 variable "allowsts_group" {
+  description = "(optional) The group to allow access to the organization, defaults to: terraform-access"
   type    = string
   default = "terraform-access"
 }
 
 variable "access_role" {
+  description = "(optional) The role to allow access to the organization, defaults to: TerraformRole"
   type    = string
   default = "TerraformRole"
 }
