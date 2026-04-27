fc3e16823aea17389ee6468480b5981141134b5f
Cristian Beraha <cristian@cloudops.works>
2026-04-27 09:25:37 -0300

    chore: Upgrade repository from template, version: v1.6.27

--- versions.tf
+++ versions.tf
@@ -1,14 +1,19 @@
 ##
-# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
-#            On GitHub: https://github.com/cloudopsworks
-#            Distributed Under Apache v2.0 License
+# (c) 2021-2026
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
 #
+
 terraform {
-  required_version = ">= 1.2"
+  required_version = ">= 1.3"
+  # Complete with required providers for the module
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.4"
     }
   }
 }
